75f3e5b8279d18a63d5fed79ecec6772ba06a368
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-21 09:50:25 -0300

    Adcionado animação no projeto

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,71 +1,66 @@
+import { useState, useCallback } from "react";
+import { FaGithub,FaPlus,FaSpinner  } from "react-icons/fa";
+import { Container, Form, SubmitButton} from "./styles";
 
-import {useState,useCallback} from 'react'
-import {FaGithub,FaPlus} from 'react-icons/fa'
-import {Container,Form,SubmitButton} from './styles'
+import api from "../../services/api";
 
-import api from '../../services/api'
+export default function Main() {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositorios, setRepositorios] = useState([]);
+  const [loading,setLoading] = useState(false)
 
 
-export default function Main(){
-  const [newRepo,setNewRepo] = useState('')
+    const handleSubmit = useCallback((e) => {
+            e.preventDefault();
+            async function submit() {
+                setLoading(true);
+                try {
+                    const response = await api.get(`repos/${newRepo}`);
 
-  const [repositorios,setRepositorios] = useState([''])
+                    const data = {
+                        name: response.data.full_name,
+                    };
+                    setRepositorios([...repositorios, data]);
+                    setNewRepo('');
+                    
+                } catch (error) {
+                    console.log(error)
+                    
+                }finally{
+                 setLoading(false)
+                }
+            }
+             
+            submit();
+        },[newRepo, repositorios]);
 
-     const handleSubmit = useCallback((e)=>{
-         e.preventDefault()
-    async function submit(){
-        const response = await api.get(`repos/${newRepo}`);
-         
-        const data ={
-            name:response.data.full_name,
-        }
-            setRepositorios([...repositorios,data])
-            setNewRepo('');
-    }
-
-    submit()
-
-      
-     },[newRepo,repositorios]);
- 
-      
-
-   
-     
-      
-
-  
-
-  function handleinputChange(e){
-      setNewRepo(e.target.value);
-      
-
+  function handleinputChange(e) {
+    setNewRepo(e.target.value);
   }
 
-    return(
-        
-     <Container>
-         <h1>
-             <FaGithub size={25}/>
-          Meus Repositorios
-        </h1>
-        
-        <Form onSubmit={handleSubmit}>
+  return (
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositorios
+      </h1>
 
-            <input type="text" 
-            placeholder="Adcionar Repositorios" 
-            value={newRepo}
-            onChange={handleinputChange}
-            
-            />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adcionar Repositorios"
+          value={newRepo}
+          onChange={handleinputChange}
+        />
 
-            <SubmitButton>
-             <FaPlus color="#fff" size={14}/>
-            </SubmitButton>
-        </Form>
-
-        
-     </Container>
-        
-    )
+        <SubmitButton loading={loading ? 1 : 0}>
+            {loading ?(
+                <FaSpinner color="#fff" size={14}/> 
+            ):(
+                <FaPlus color="#fff" size={14} />
+            )}
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,62 +1,75 @@
-import styled from 'styled-components'
+import styled,{keyframes,css} from "styled-components";
 
 export const Container = styled.div`
-max-width: 700px;
-background: #fff;
-border-radius: 4px;
-box-shadow: 0 0 20px rgba(0,0,0 0.2);
-padding: 30px;
-margin: 80px auto;
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0 0.2);
+  padding: 30px;
+  margin: 80px auto;
 
-h1{
+  h1 {
     font-size: 20px;
     display: flex;
     align-items: center;
     flex-direction: row;
 
-    svg{
-        margin-right:10px ;
+    svg {
+      margin-right: 10px;
     }
-
-
-}
-
-
-
-
+  }
 `;
 
 export const Form = styled.form`
-margin-top: 30px;
-display: flex;
-flex-direction: row;
-input{
-    flex:1;
-    border: 1px solid #DDD;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+  input {
+    flex: 1;
+    border: 1px solid #ddd;
     padding: 10px 15px;
     border-radius: 4px;
     font-size: 17px;
+  }
+`;
+
+// Criando animação do butao
+
+const animate = keyframes`
+  from{
+      transform: rotate(0deg);
+  }
+  to{
+      transform: rotate(360deg);
+  }
+`
+
+export const SubmitButton = styled.button.attrs(props =>({
+  type: "submit",
+  disabled:props.loading,
+}))`
+  background: #0d2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+&[disabled]{
+    cursor: not-allowed;
+    opacity: 0.5;
 }
 
+${props=>props.loading &&
+css`
+ svg{
+     animation: ${animate} 2s linear infinite;
+ }
 
+`
 
-
-`;
-
-export const SubmitButton = styled.button.attrs({
-    type:'submit'
-})`
-background: #0D2636;
-border: 0;
-border-radius: 4px;
-margin-left: 10px;
-padding: 0 15px;
-display: flex;
-justify-content: center;
-align-items:center;
-
-
-
+}
 
 `;
-
