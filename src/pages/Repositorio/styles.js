19b429862aee322f29559e65a47af11bf7e31230
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-21 18:02:19 -0300

    Criando paginação

--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -117,4 +117,24 @@ strong{
 }
 
 `
+export const PageAction = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+button{
+    outline: 0;
+    border: 0;
+    background: #222;
+    color: #fff;
+    padding: 5px 10px;
+    border-radius: 4px;
+
+    &:disabled{
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+}
+
+`;
 
